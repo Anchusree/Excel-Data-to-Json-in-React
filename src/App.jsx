a5cae8633e5703dffcb6d1bed53d82ff78c8bc93
Anchusree <=>
2023-07-21 00:09:59 +0300

    added app.js

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { useRef, useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as XLSX from 'xlsx'
+
+function App() {
+
+  const [data, setData] = useState([])
+  const [fileName,setFileName] = useState('')
+
+  const fileRef = useRef()
+
+
+  //check valid excel file
+  const isExcelFile = (file) => {
+    const allowedExtensions = ['.xlsx', '.xls']
+    const fileName = file.name
+    const fileExtension = fileName.slice(fileName.lastIndexOf('.')).toLowerCase()
+    return allowedExtensions.includes(fileExtension)
+
+  }
+
+  const handleFile = (e) => {
+    const file = e.target.files[0]
+    if (!file) return
+
+    if (isExcelFile(file) && file.size != 0) {
+      const reader = new FileReader()
+      reader.readAsBinaryString(file)
+      reader.onload = (e) => {
+        const data1 = e.target.result
+        const workbook = XLSX.read(data1, { type: "binary" })
+        const sheetName = workbook.SheetNames[0]
+        const sheet = workbook.Sheets[sheetName]
+        const parsedData = XLSX.utils.sheet_to_json(sheet, { defval: null })
+        //console.log(parsedData);
+        setData(parsedData)
+      }
+      setFileName(file.name)
+    }
+    else {
+      alert("Invalid file format")
+    }
+
+  }
+
+  const handleRemoveFile = ()=>{
+    setFileName(null)
+    fileRef.current.value =""
+    setData([])
+  }
+
+  return (
+    <div>
+      <h1>Excel React</h1>
+      <br />
+
+      <div className="mb-3">
+        <label htmlFor="formFile" className="form-label">Upload file</label>
+        <input className="form-control" type="file" id="formFile" accept=".xlsx,.xls" onChange={handleFile} ref={fileRef} />
+        {
+          fileName && (
+            <>
+            <span>{fileName}</span>
+            <button className='removeFile' onClick={handleRemoveFile}>X</button>
+            </>
+          )
+        }
+      </div>
+
+      <br />
+      {
+        data && data.length > 0 && (
+          <div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                {
+                  Object.keys(data[0]).map((key)=>(
+                    <th scope="col" key={key}>{key}</th>
+                  ))
+                }
+              
+              </tr>
+            </thead>
+            <tbody>
+                {
+                  data.map((row,index)=>(
+                    <tr key={index}>
+                      {Object.values(row).map((value,index)=>(
+                        <td key={index}>{value}</td>
+                      ))}
+                    </tr>
+                  ))
+                }
+             
+           
+            </tbody>
+          </table>
+        </div>
+        )
+      }
+     
+
+
+
+
+    </div>
+  )
+}
+
+export default App
